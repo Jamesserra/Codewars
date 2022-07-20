989f253e1e7f5f6dcd8172352f9376a3531a225f
jamesserra <jamesserra4@gmail.com>
2022-07-20 17:45:51 -0400

    solutions

---
+++ 6-kyu/Validate-The-Credit-Card-Number.js
@@ -0,0 +1,69 @@
+// In this Kata, you will implement the Luhn Algorithm, which is used to help validate credit card numbers.
+
+// Given a positive integer of up to 16 digits, return true if it is a valid credit card number, and false if it is not.
+
+// Here is the algorithm:
+
+// Double every other digit, scanning from right to left, starting from the second digit (from the right).
+
+// Another way to think about it is: if there are an even number of digits, double every other digit starting with the first; if there are an odd number of digits, double every other digit starting with the second:
+
+// 1714 ==> [1*, 7, 1*, 4] ==> [2, 7, 2, 4]
+
+// 12345 ==> [1, 2*, 3, 4*, 5] ==> [1, 4, 3, 8, 5]
+
+// 891 ==> [8, 9*, 1] ==> [8, 18, 1]
+// If a resulting number is greater than 9, replace it with the sum of its own digits (which is the same as subtracting 9 from it):
+
+// [8, 18*, 1] ==> [8, (1+8), 1] ==> [8, 9, 1]
+
+// or:
+
+// [8, 18*, 1] ==> [8, (18-9), 1] ==> [8, 9, 1]
+// Sum all of the final digits:
+
+// [8, 9, 1] ==> 8 + 9 + 1 = 18
+// Finally, take that sum and divide it by 10. If the remainder equals zero, the original credit card number is valid.
+
+// 18 (modulus) 10 ==> 8 , which is not equal to 0, so this is not a valid credit card number
+
+function validate(n) {
+  let copy = Array.from(String(n), Number);
+  let array = [];
+  let sum = 0;
+
+  function split(number) {
+    return number
+      .toString()
+      .split("")
+      .reduce((a, b) => parseInt(a) + parseInt(b), 0);
+  }
+  function sumArray(array) {
+    sum = array.reduce((a, b) => a + b, 0);
+    return sum % 10 === 0 ? true : false;
+  }
+
+  if (copy.length % 2 === 0) {
+    copy.forEach((number, index) => {
+      if (index % 2 === 0) {
+        let result = number * 2;
+        result > 9 ? array.push(split(result)) : array.push(result);
+      } else {
+        array.push(number);
+      }
+    });
+    return sumArray(array);
+  }
+
+  if (copy.length % 2 !== 0) {
+    copy.forEach((number, index) => {
+      if (index % 2 !== 0) {
+        let result = number * 2;
+        result > 9 ? array.push(split(result)) : array.push(result);
+      } else {
+        array.push(number);
+      }
+    });
+    return sumArray(array);
+  }
+}
